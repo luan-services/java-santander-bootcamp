41365c387c0a597f4a8f5b5bc0dd22436cb9aa2c
Luan Peixoto <luandotservices@gmail.com>
2026-07-26 15:56:24 -0300

    feat: study update

--- first-java-project/src/App.java
+++ first-java-project/src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+import test.TestClass;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.next();
+        int age = scanner.nextInt();
+
+        System.out.println(name + age);
+        scanner.close();
+
+        TestClass t = new TestClass();
+        t.runTest(67);
     }
 }
---
+++ first-java-project/src/test/TestClass.java
@@ -0,0 +1,11 @@
+package test;
+
+public class TestClass {
+
+    public static void runTest(int number) {
+        System.out.printf("Test done! %d", number);
+    }
+    public static void main(String[] args) {
+        
+    }
+}
